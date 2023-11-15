5e250900c17e6c53a3e7d7eec5bf09e97a3c3dbe
TopHatGuy12 <85897769+TopHatGuy12@users.noreply.github.com>
2023-11-15 15:31:57 -0700

    fgtu

---
+++ student.h
@@ -0,0 +1,17 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+#include <vector>
+
+
+
+class Student{
+public:
+    
+private:
+    std::string name;
+    int id;
+    std::vector<int> grades;
+};
+
+#endif
